914b361c397984b6e2728c372406d9dd03f6077b
jlmin <lmk264509@gmail.com>
2025-06-07 18:45:05 +0900

    메인파일 추가

---
+++ calc/calc.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include "add.h"
+#include "dive.h"
+#include "mod.h"
+#include "mul.h"
+#include "sub.h"
+int main()
+{
+    int x = 0;
+    int y = 0;
+
+    std::cout << "첫번쨰 수를 입력하세요 : ";
+    std::cin >> x;
+    std::cout << "두번쨰 수를 입력하세요 : ";
+    std::cin >> y;
+
+    printf("입력된 수 x[ %d ] y[ %d ]\n", x, y);
+    printf("add[ %d ] sub[ %d ] mul[ %d ] dive[ %d ] mod[ %d ]\n",
+        add(x, y), sub(x, y), mul(x, y), dive(x, y), mod(x, y));
+}
